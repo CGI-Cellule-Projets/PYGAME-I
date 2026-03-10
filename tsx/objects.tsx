98a7443ad019d778702ca60f5a30ccfdacd71f55
Zorojuro-01 <zerki.medsaad@gmail.com>
2026-03-10 16:41:50 +0000

    Add files via upload

---
+++ tsx/objects.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="objects" tilewidth="256" tileheight="256" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../Users/zerki/Downloads/tree_3.png" width="256" height="256"/>
+ </tile>
+ <tile id="1">
+  <image source="../Users/zerki/Downloads/tree_2.png" width="256" height="256"/>
+ </tile>
+ <tile id="2">
+  <image source="../Users/zerki/Downloads/tree_1.png" width="256" height="256"/>
+ </tile>
+ <tile id="3">
+  <image source="../Users/zerki/Downloads/mushroom_3.png" width="64" height="64"/>
+ </tile>
+ <tile id="4">
+  <image source="../Users/zerki/Downloads/mushroom_2.png" width="64" height="64"/>
+ </tile>
+ <tile id="5">
+  <image source="../Users/zerki/Downloads/mushroom_1.png" width="64" height="64"/>
+ </tile>
+</tileset>
